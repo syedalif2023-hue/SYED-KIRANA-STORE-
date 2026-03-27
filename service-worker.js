00bb9e09bcbc555a8ccc0c88dab1951954c5fa37
syedalif2023-hue <syedalif2023@gmail.com>
2026-03-27 14:10:06 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,35 @@
-const cacheName = 'bazar-v1';
-const staticAssets = [
+const CACHE_NAME = 'grocery-v1';
+const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  'https://i.postimg.cc/j57H1PP8/file-0000000062fc7208bcb58f5264a5b1d7.png'
 ];
 
-// Install Event
-self.addEventListener('install', async event => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
+// इंस्टॉल इवेंट - फाइलों को कैश करना
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-// Fetch Event (Offline support)
-self.addEventListener('fetch', event => {
+// एक्टिवेट इवेंट - पुरानी कैश हटाना
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// फेच इवेंट - ऑफलाइन होने पर कैश से फाइल देना
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
